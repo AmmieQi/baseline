a33c19454c604442cd3f2992c0769421c7c26417
WencanHuang <2468874252@qq.com>
2019-09-30 01:36:43 +0800

    prepare

---
+++ data/prepare.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+VideoAnnotationPath_training = "/home1/huangwencan/training"
+VideoAnnotationPath_validation = "/home1/huangwencan/validation"
+VidorFeaturePath = "/home1/zhaoyang/dataset/vidor_feature"
+VidorAnnotationPath = "/home1/zhaoyang/dataset/vidor_annotation"
+CSVPath = "/home1/zhaoyang/dataset/vidor_csv"
+
